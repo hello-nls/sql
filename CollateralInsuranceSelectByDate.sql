9cb5cd45d5e993c3afa56b8859439ef89b0bd588
sdougwillo <40274414+sdougwillo@users.noreply.github.com>
2018-07-31 17:28:14 -0400

    Update CollateralInsuranceSelectByDate.sql
    Updated version to link in the loanacct_collateral table and allow selection based on collateral types.

--- CollateralInsuranceSelectByDate.sql
+++ CollateralInsuranceSelectByDate.sql
@@ -1,6 +1,9 @@
 SELECT l.acctrefno, CONVERT(date, policy_expiration_date), CONVERT(date, GETDATE()), ci.*
-FROM collateral_insurance ci, loanacct l, loanacct_collateral_link lcl
-WHERE CONVERT(date, policy_expiration_date) = CONVERT(date, GETDATE())
+FROM collateral_insurance ci, loanacct l, loanacct_collateral_link lcl, loanacct_collateral lc
+WHERE CONVERT(date, policy_expiration_date) <= CONVERT(date, GETDATE())
 and l.acctrefno = lcl.acctrefno
 and lcl.collateral_id = ci.collateral_id
+and lcl.collateral_id = lc.collateral_id
 AND status_code_no = 0
+AND (collateral_code = 4
+OR collateral_code = 5)
