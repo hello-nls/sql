c36a560b67a4e3b4cb8ce6d039f947dcc19f8d24
Jojo <40130858+jojonls@users.noreply.github.com>
2018-08-01 13:53:09 -0700

    Update CollateralInsuranceSelectByDate.sql

--- CollateralInsuranceSelectByDate.sql
+++ CollateralInsuranceSelectByDate.sql
@@ -1,3 +1,17 @@
+/*
+ * CollateralInsuranceSelectByDate
+ */
+SELECT l.acctrefno, CONVERT(date, policy_expiration_date), CONVERT(date, GETDATE()), ci.*
+FROM collateral_insurance ci, loanacct l, loanacct_collateral_link lcl
+WHERE CONVERT(date, policy_expiration_date) = CONVERT(date, GETDATE())
+and l.acctrefno = lcl.acctrefno
+and lcl.collateral_id = ci.collateral_id
+AND status_code_no = 0
+
+
+/*
+ * SelectByDateAndCollateralType
+ */
 SELECT l.acctrefno, CONVERT(date, policy_expiration_date), CONVERT(date, GETDATE()), ci.*
 FROM collateral_insurance ci, loanacct l, loanacct_collateral_link lcl, loanacct_collateral lc
 WHERE CONVERT(date, policy_expiration_date) <= CONVERT(date, GETDATE())
