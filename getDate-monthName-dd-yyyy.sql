14bcc9af4819e06f8489a66dbe920a59b8bda79d
Jojo <40130858+jojonls@users.noreply.github.com>
2018-08-01 13:59:03 -0700

    Update getDate-monthName-dd-yyyy.sql

--- getDate-monthName-dd-yyyy.sql
+++ getDate-monthName-dd-yyyy.sql
@@ -1,10 +1,10 @@
--- example: July 28, 2018 
+/* example: July 28, 2018 */
 SELECT DATENAME(MONTH, SYSDATETIME())+ ' ' 
            + RIGHT('' + DATENAME(DAY, SYSDATETIME()), 2) + ', ' 
                       + DATENAME(YEAR, SYSDATETIME()) 
 
 
--- example: July 28, 2018
+/* example: July 28, 2018 */
 SELECT DATENAME(MONTH, GETDATE())+ ' ' 
            + RIGHT('' + DATENAME(DAY, GETDATE()), 2) + ', ' + 
                         DATENAME(YEAR, GETDATE()) 
