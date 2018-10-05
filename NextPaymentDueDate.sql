13a51cb23faf6fb2f602d6f1469fe8979c398006
Jojo <40130858+jojonls@users.noreply.github.com>
2018-10-05 13:04:02 -0700

    Update NextPaymentDueDate.sql

--- NextPaymentDueDate.sql
+++ NextPaymentDueDate.sql
@@ -1,4 +1,6 @@
-
+-- 
+-- https://community.nortridge.com/t/next-payment-due-date/1751 
+--
 	select
 		case when (select min(date_due) from loanacct_payments_due where acctrefno = 1) is null  
 		then 
