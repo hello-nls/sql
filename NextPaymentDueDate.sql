e48f73bd4a5d032d5acec5102afc1fd7df6b7629
Jojo <40130858+jojonls@users.noreply.github.com>
2018-10-05 13:03:20 -0700

    Create NextPaymentDueDate.sql

---
+++ NextPaymentDueDate.sql
@@ -0,0 +1,8 @@
+
+	select
+		case when (select min(date_due) from loanacct_payments_due where acctrefno = 1) is null  
+		then 
+			(select next_principal_payment_date from loanacct_payment P where P.acctrefno = 1) 
+		else 
+			(select min(date_due) from loanacct_payments_due where acctrefno = 1)
+		end
