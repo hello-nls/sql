02441597bc9b9cb4a812f4cd0428294bac737f02
Jojo <40130858+jojonls@users.noreply.github.com>
2018-11-28 11:21:12 -0800

    Update listAccountsEmailHistory.sql

--- listAccountsEmailHistory.sql
+++ listAccountsEmailHistory.sql
@@ -1,7 +1,22 @@
-/*   from the community site
+/*   From the community site: 
         https://community.nortridge.com/t/find-what-account-an-email-was-sent-to-from-table-emailhistory/1898/11 
+           Q by rpaul
+           A by robert.buchanan
 */
 
+-- From Robert 
+SELECT        row_id, acctrefno, comments
+FROM            dbo.loanacct_comments
+Where loanacct_comments.comments  like '%EmailHistoryID%'
+
+
+-- From Ryan
+SELECT row_id, acctrefno, comments
+FROM nls.dbo.loanacct_comments
+Where loanacct_comments.comments like ‘%EmailHistoryID%’
+
+
+-- From Jojo
 SELECT  l.acctrefno, l.loan_number, c.created   
 FROM  loanacct_comments c, loanacct l
 where l.acctrefno = c.acctrefno 
