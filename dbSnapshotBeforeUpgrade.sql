1f5b89df6f8a9f2cc7d1e4fd6bb23c48c93c7257
Jojo <40130858+jojonls@users.noreply.github.com>
2018-11-29 15:32:45 -0800

    Update dbSnapshotBeforeUpgrade.sql

--- dbSnapshotBeforeUpgrade.sql
+++ dbSnapshotBeforeUpgrade.sql
@@ -1,3 +1,8 @@
+/* 
+ * Reference: 
+ *            https://community.nortridge.com/t/rollback-script-within-nls/1863/14 
+ */
+
 CREATE DATABASE t1_snapshot ON
 ( NAME = t1, FILENAME = 'E:\Temp\DB-Snapshot\t1\snapshot.bak')
 AS SNAPSHOT of t1
