54b46baddeeb462fc4336710baeb742548b36549
Jojo <40130858+jojonls@users.noreply.github.com>
2019-01-17 14:49:38 -0800

    Update getDate-mm-dd-yyyy.sql

--- getDate-mm-dd-yyyy.sql
+++ getDate-mm-dd-yyyy.sql
@@ -3,3 +3,10 @@ select CONVERT(varchar(10),getdate(),101)
 
 /* example: 07/28/2018 */
 select CONVERT(varchar(10),SYSDATETIME(),101)
+
+
+
+-- from a UDF 
+select CONVERT(varchar(10),CAST(userdef26 AS date),101)
+from loanacct_detail
+where acctrefno = 36280
