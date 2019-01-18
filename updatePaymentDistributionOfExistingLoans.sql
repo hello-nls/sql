4bfaf6f429cf6765d5b8cf5c8ad25f68e6d6e3aa
Jojo <40130858+jojonls@users.noreply.github.com>
2019-01-18 12:34:40 -0800

    Update updatePaymentDistributionOfExistingLoans.sql

--- updatePaymentDistributionOfExistingLoans.sql
+++ updatePaymentDistributionOfExistingLoans.sql
@@ -26,3 +26,21 @@ where acctrefno in (
 		select loan_group_no from loan_group where loan_group = ‘DBHIL’
 	)
 )
+
+
+
+
+-- from jojo (version 2)
+update  loanacct_setup 
+set payment_distribution_order = (
+		select loan_template.payment_distribution_order 
+		from loan_template 
+		where loan_template.loan_template_no = 123456
+	)
+where acctrefno in (
+	select acctrefno 
+	from loanacct 
+	where loan_group_no in (
+		select loan_group_no from loan_group where loan_group = 'DBHIL'
+	)
+)
