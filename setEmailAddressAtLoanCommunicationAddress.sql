c18be597acbb39451ca086df711d5a4738f585a9
Jojo <40130858+jojonls@users.noreply.github.com>
2019-01-22 11:04:05 -0800

    Create setEmailAddressAtLoanCommunicationAddress.sql

---
+++ setEmailAddressAtLoanCommunicationAddress.sql
@@ -0,0 +1,17 @@
+/*
+   Source: https://community.nortridge.com/t/communication-preferences-automation/2032  
+*/
+
+-- get COMMID
+select LoanCommunicationPreferencesID 
+from loancommunicationpreferences  
+where acctrefno = REFNO
+
+-- set the email option
+update loancommunicationpreferences 
+set StatementOptionFlags = 10 
+where LoanCommunicationPreferencesID = COMMID
+
+-- set the email address
+insert into LoanCommunicationAddress (LoanCommunicationPreferencesID, Address, AddressType) 
+values (COMMID, BuyerEmail, 0)
