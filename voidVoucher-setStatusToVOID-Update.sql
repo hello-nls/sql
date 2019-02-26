12e4528d52f6541d4491bf8c87165c766ad519e5
Jojo <40130858+jojonls@users.noreply.github.com>
2019-02-25 23:11:38 -0800

    Update voidVoucher-setStatusToVOID-Update.sql

--- voidVoucher-setStatusToVOID-Update.sql
+++ voidVoucher-setStatusToVOID-Update.sql
@@ -1,6 +1,12 @@
 /*
  *  from the community site 
- *      https://community.nortridge.com/t/unable-to-reverse-a-duplicate-transaction-and-also-void-the-associated-voucher/2064  
+ *    https://community.nortridge.com/t/unable-to-reverse-a-duplicate-transaction-and-also-void-the-associated-voucher/2064  
+ *      Voucher_Status:
+ *      0 Pending
+ *      1 Paid
+ *      2 Voided
+ *      3 Hold
+ *      4 Queued
  */
 
 -- Set Voucher Status to VOID 
